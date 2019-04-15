2f848586d846b587ddb1396a4c71cfc4b45ebece
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-15 02:48:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array.join
   elsif array.count == 2
     array.join(" and ")
-  elsif array.count == 3
+  elsif array.count >= 3
     i = array.pop
       a = array.join(", ")
       "#{a}, and #{i}"
